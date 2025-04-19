33477cfd2b78576f01fecb07f3b2531cae1b55e6
smixs <mixshima@gmail.com>
2025-04-19 13:27:21 +0500

    black!

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="dark scroll-smooth">
       <body className={inter.className}>
         {children}
         <BackToTop />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,8 +16,8 @@ const config: Config = {
       },
       borderRadius: {
         lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        md: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 8px)',
       },
       colors: {
         background: 'hsl(var(--background))',
